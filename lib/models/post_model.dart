0c100198a5c312c2f314d35a1c8744a639d28f94
tanatron <Basg344@gmail.com>
2025-09-28 23:22:40 +0700

    userId

--- lib/models/post_model.dart
+++ lib/models/post_model.dart
@@ -1,13 +1,19 @@
-// lib/post_model.dart
 class Post {
+  final int userId;
   final int id;
   final String title;
   final String body;
 
-  const Post({required this.id, required this.title, required this.body});
+  const Post({
+    required this.userId,
+    required this.id,
+    required this.title,
+    required this.body,
+  });
 
   factory Post.fromJson(Map<String, dynamic> json) {
     return Post(
+      userId: json['userId'] as int,
       id: json['id'] as int,
       title: json['title'] as String,
       body: json['body'] as String,
